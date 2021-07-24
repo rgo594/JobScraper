7f2fc0b5dd594f193e610bbbdd1c4ae20012a013
Rob O'Gara <rgo594@gmail.com>
2021-07-24 18:53:54 -0400

    JobScraper

---
+++ JobScraper/JobScraper/spiders/jobs_spider.py
@@ -0,0 +1,23 @@
+import scrapy
+
+
+class JobsSpider(scrapy.Spider):
+    name = "jobs"
+    start_urls = [
+        # 'https://mangaread.co/manga/kanojo-okarishimasu/'
+        'https://www.indeed.com/jobs?q=data%20engineer&l',
+    ]
+
+
+    def parse(self, response):
+        for job in response.xpath('.//div[@class="job_seen_beacon"]'):
+            yield{
+                'position': job.xpath('.//h2[contains(@class,"jobTitle")]//span[@title]/text()').getall(),
+                'company': job.xpath('.//a[@data-tn-element ="companyName"]/text()').getall(),
+                'comp2': job.xpath('.//span[@class ="companyName"]/text()').getall(),
+                'location': job.css('div.companyLocation::text').getall(),
+            }
+
+        next_page = response.xpath('//a[@aria-label="Next"]/@href').get()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
